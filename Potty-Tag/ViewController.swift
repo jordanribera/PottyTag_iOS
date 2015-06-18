299e4f7da0931061a0b9cf4d78209b876571bf82
Admin <admin@Johns-MacBook-Pro.local>
2015-06-18 11:30:03 -0700

    Initial Commit

---
+++ Potty-Tag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Potty-Tag
+//
+//  Created by Admin on 6/18/15.
+//  Copyright (c) 2015 Tag Creative Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
